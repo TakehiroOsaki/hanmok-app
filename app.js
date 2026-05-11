f513ebf0c2388a9c77d7974dd9ba7ab9d8908bf9
Takehiro Osaki <69943630+TakehiroOsaki@users.noreply.github.com>
2026-05-11 17:18:54 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1049,6 +1049,6 @@ async function doExport() {
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('sw.js').catch(()=>{});
   caches.keys().then(keys => {
-    keys.filter(k => k !== 'hanmok-v16').forEach(k => caches.delete(k));
+    keys.filter(k => k !== 'hanmok-v17').forEach(k => caches.delete(k));
   });
 }
